0502d871cbf69227abf9920e3bbb055897aac15e
Aditya Acharya <aditiya.acharya910@gmail.com>
2024-01-25 13:47:11 +0545

    bit wise operation example

---
+++ bit_wise_operation.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int num1, num2;
+    printf("Enter any two number: ");
+    scanf("%d %d", &num1, &num2);
+    int bitAnd, bitOr, bitXor, bitNot1, bitNot2, bitLshift, bitRshift;
+    bitAnd = num1 & num2;
+    bitOr = num1 | num2;
+    bitXor = num1 ^ num2;
+    bitNot1 = ~num1;
+    bitNot2 = ~num2;
+    bitLshift = num1 << 2;
+    bitRshift = num2 >> 2;
+    printf("Bitwise And: %d\n", bitAnd);
+    printf("Bitwise OR : %d\n", bitOr);
+    printf("Bitwise XOR: %d\n",bitXor);
+    printf("Bitwise Not for 1st number: %d\n",bitNot1);
+    printf("Bitwiset Not for 2nd number: %d\n", bitNot2);
+    printf("Bitwise Left Shift: %d\n", bitLshift);
+    printf("Bitwise Right Shift: %d\n", bitRshift);
+    return 0;
+}
