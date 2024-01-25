cf209a68581ebadd1c9ef521821773a6fad6fd6c
Aditya Acharya <aditiya.acharya910@gmail.com>
2024-01-25 14:08:31 +0545

    LAB Assignments

---
+++ c_assignments.c
@@ -0,0 +1,22 @@
+// LAB 9
+#include <stdio.h>
+
+// int main(){
+    // printf("Name: Aditya Acharya\nSection: \"A\"");
+//     int anyVar;
+//     anyVar = 9;
+//     printf("%d", anyVar);
+// }
+// Lab 10
+int main(){
+    int anyNum;
+    printf("Name: Aditya Acharya\nSection: \"A\"\n");
+    printf("Enter any number: ");
+    scanf("%d", &anyNum);
+    printf("++ operation after the variable(a++) input number is: %d\n", anyNum++);
+    printf("++ operation before the variable(++a) input number is: %d\n", ++anyNum);
+    printf("-- operation after the variable(a--) input number is: %d\n", anyNum--);
+    printf("-- operation before the variable(--a) input number is: %d\n", --anyNum);
+    
+
+}
