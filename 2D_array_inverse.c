8f2302c9bf74d88c843b8f68bb737199d5b1f225
Aditya Acharya <aditiya.acharya910@gmail.com>
2024-02-02 08:11:40 +0545

    About 2D array

--- 2D_array_inverse.c
+++ 2D_array_inverse.c
@@ -9,6 +9,9 @@ int main()
     // int Arr[3][4] = {{1, 2, 3, 4},
     //                  {5, 6, 7, 8},
     //                  {9, 10, 11, 12}};
+    // printf("Enter the order of matrix you want to enter(Row Column): ");
+    // scanf("%d %d", &row, &column);
+    // Arr[row][column];
     int i,j;
     for(i=0; i<row; i++){
         for(j=0; j<column; j++){
