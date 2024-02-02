af30daade697d08e27ab3e104002cf534d6d06b0
Aditya Acharya <aditiya.acharya910@gmail.com>
2024-02-02 08:16:40 +0545

    problem number 27 lab report

---
+++ problem/problem_27.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    // printf("%d", i);
+    while (i < 10)
+    {
+        
+        if (i == 5)
+        {
+            // printf("%d" ,i);
+            continue;
+        }
+        else
+        {
+            printf("%d", i);
+        }
+        i++;
+    }
+}
+
+// WORKING
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     while (i < 10)
+//     {
+//         i++;
+//         if (i == 5)
+//         {
+//             continue;
+//         }
+//         else
+//         {
+//             printf("%d\n", i);
+//         }
+        
+        
+//     }
+// }
+
+
+// 5 MA LOOP CONTINUE BHAYERA I KO VALUE BADHNA PAYENA
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     while (i < 10)
+//     {
+//         i++;
+//         if (i == 5)
+//         {
+//             continue;
+//         }
+//         else
+//         {
+//             printf("%d\n", i);
+//         }
+        
+        
+//     }
+// }
